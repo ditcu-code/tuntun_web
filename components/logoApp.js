96491b3ea8d5961437f35197b2504bd497265ab8
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-12-07 21:44:48 +0700

    first commit

---
+++ components/logoApp.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Image from "next/image"
+import logoImg from "../resources/tuntunLogo.jpg"
+import Link from "next/link"
+
+export default function LogoApp({ width }) {
+  console.log("width", width)
+  return (
+    <Link href={"/"}>
+      <Image src={logoImg} alt={"tuntun-logo"} width={width} />
+    </Link>
+  )
+}
