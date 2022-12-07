8e9b5f0d4f3d8e38609b3fb43544b35566a1feea
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-12-07 23:03:28 +0700

    update responsive

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <LogoApp width={500} />
+        <LogoApp width={"300"} />
 
         <div className={styles.grid}>
           <Link href="/terms-and-conditions" className={styles.card}>
@@ -28,19 +28,6 @@ export default function Home() {
           </Link>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
