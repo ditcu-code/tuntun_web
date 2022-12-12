ec195272da520c840183e52c8d73ca90985d0dd7
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-12-12 16:53:16 +0700

    add events

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ import appstore from "../resources/appstoreLabel.png"
 import { event } from "../lib/gtag"
 
 export default function Home() {
-  function handleVisitAppStore() {
+  function handleVisit(actionName) {
     event({
-      action: "AppStore",
+      action: actionName,
       category: "Links",
       label: "AppStore logo clicked!",
       value: 1,
@@ -26,7 +26,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <LogoApp width={"300"} />
-        <Link href="/appstore" onClick={handleVisitAppStore}>
+        <Link href="/appstore" onClick={handleVisit("home_to_appstore")}>
           <Image
             className={styles.appstoreLabel}
             src={appstore}
@@ -35,11 +35,19 @@ export default function Home() {
         </Link>
 
         <div className={styles.grid}>
-          <Link href="/terms-and-conditions" className={styles.card}>
+          <Link
+            href="/terms-and-conditions"
+            className={styles.card}
+            onClick={handleVisit("home_to_terms")}
+          >
             <p>Terms & Condition</p>
           </Link>
 
-          <Link href="/privacy-policy" className={styles.card}>
+          <Link
+            href="/privacy-policy"
+            className={styles.card}
+            onClick={handleVisit("home_to_privacy")}
+          >
             <p>Privacy Policy</p>
           </Link>
         </div>
